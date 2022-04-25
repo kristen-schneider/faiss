e1921466b21baafd8040ba0a12dc087ec03ee0f3
kristen-schneider <kristen.schneider@colorado.edu>
2022-04-25 15:17:37 -0600

    count and percent for shared sites good

---
+++ tutorial/kristen/scripts/shared_sites.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+def count_shared_sites(query, match):
+    '''
+    counts number of shared sites between a match and a query
+    '''
+    shared_sites = 0
+    for s in range(len(query)):
+        if query[s] == match[s]:
+            shared_sites += 1
+
+    return shared_sites
+
+def percent_shared_sites(query, match):
+    '''
+    converts number of shared sites to a percentage
+    '''
+    num_shared_sites = count_shared_sites(query, match)
+    num_sites = len(query)
+    return 1-(num_shared_sites/num_sites)
+
+def all_matches(all_queries, all_matches, smf):
+    '''
+    returns a list of lists, one list for each query
+    one list is the percent matches for all index matches
+    '''
+    for q in range(len(all_queries)):
+        q_query = all_queries[q]
+        q_matches = all_matches[q]
+        for m in range(len(q_matches)):
+            m_sample = smf[q_matches[m]]
+            p_ss = percent_shared_sites(q_query, m_sample)
+
+            #print(p_ss)
+
+def all_shared_sites(queries, full_database):
+    '''
+    returns a list of lists, one list for each query
+    one list is the percent matches for all samples in database
+    '''
+    accuracy_list = []
+
+    for query_i in range(len(queries)):
+        curr_query = queries[query_i]
+        for sample_i in range(len(full_database)):
+            curr_sample = full_database[sample_i]
+            p_ss = percent_shared_sites(curr_query, curr_sample)
+            try: accuracy_list[query_i].append(p_ss)
+            except IndexError: accuracy_list.append([p_ss])
+    return accuracy_list
+
+def accuracy_indices(accuracty_list):
+    '''
+    sorts list of accuracy percentages and returns the indexes of the ordered list
+    '''
+    ordered_indices = []
+    for l in accuracty_list:
+        index_ordered = np.argsort(l)
+        ordered_indices.append(index_ordered)
+    return ordered_indices
+
+def ss_vs_bf(ss_indices, bf_indices, k):
+    print('SS')
+    for ss in ss_indices:
+        print(ss[:k])
+
+    print('BF')
+    for bf in bf_indices:
+        print(bf[:k])
--- tutorial/kristen/tests/test_shared_sites.py
+++ tutorial/kristen/tests/test_shared_sites.py
@@ -1,69 +1,53 @@
+# imports
+import unittest
+import sys, os, inspect
 import numpy as np
 
-def all_matches(all_queries, all_matches, smf):
-    '''
-    returns a list of lists, one list for each query
-    one list is the percent matches for all index matches
-    '''
-    for q in range(len(all_queries)):
-        q_query = all_queries[q]
-        q_matches = all_matches[q]
-        for m in range(len(q_matches)):
-            m_sample = smf[q_matches[m]]
-            p_ss = percent_shared_sites(q_query, m_sample)
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
 
-            #print(p_ss)
+from scripts import shared_sites
 
-def percent_shared_sites(query, match):
-    '''
-    converts number of shared sites to a percentage
-    '''
-    num_shared_sites = count_shared_sites(query, match)
-    num_sites = len(query)
-    return 1-(num_shared_sites/num_sites)
+class TestSharedSites(unittest.TestCase):
+    q1 = [0, 0, 0, 0, 0]
+    q2 = [1, 1, 1, 1, 1]
+    q3 = [0, 1, 0, 1, 2]
+    m1 = [0, 0, 0, 0, 0]
+    m2 = [1, 1, 1, 1, 1]
+    m3 = [2, 2, 2, 2, 2]
 
-def count_shared_sites(query, match):
-    '''
-    counts number of shared sites between a match and a query
-    '''
-    shared_sites = 0
-    for s in range(len(query)):
-        if query[s] == match[s]:
-            shared_sites += 1
+    def test_count_shared_sites(self):
+        self.assertEqual(shared_sites.count_shared_sites(self.q1, self.m1), 5)
+        self.assertEqual(shared_sites.count_shared_sites(self.q1, self.m2), 0)
+        self.assertEqual(shared_sites.count_shared_sites(self.q1, self.m3), 0)
+        self.assertEqual(shared_sites.count_shared_sites(self.q2, self.m1), 0)
+        self.assertEqual(shared_sites.count_shared_sites(self.q2, self.m2), 5)
+        self.assertEqual(shared_sites.count_shared_sites(self.q2, self.m3), 0)
+        self.assertEqual(shared_sites.count_shared_sites(self.q3, self.m1), 2)
+        self.assertEqual(shared_sites.count_shared_sites(self.q3, self.m2), 2)
+        self.assertEqual(shared_sites.count_shared_sites(self.q3, self.m3), 1)
 
-    return shared_sites
+    def test_percent_shared_sites(self):
+        self.assertEqual(shared_sites.percent_shared_sites(self.q1, self.m1), 0.)
+        self.assertEqual(shared_sites.percent_shared_sites(self.q1, self.m2), 1.)
+        self.assertEqual(shared_sites.percent_shared_sites(self.q1, self.m3), 1.)
+        self.assertEqual(shared_sites.percent_shared_sites(self.q2, self.m1), 1.)
+        self.assertEqual(shared_sites.percent_shared_sites(self.q2, self.m2), 0.)
+        self.assertEqual(shared_sites.percent_shared_sites(self.q2, self.m3), 1.)
+        self.assertEqual(shared_sites.percent_shared_sites(self.q3, self.m1), 0.6)
+        self.assertEqual(shared_sites.percent_shared_sites(self.q3, self.m2), 0.6)
+        self.assertEqual(shared_sites.percent_shared_sites(self.q3, self.m3), 0.8)
 
-def all_shared_sites(queries, full_database):
-    '''
-    returns a list of lists, one list for each query
-    one list is the percent matches for all samples in database
-    '''
-    accuracy_list = []
 
-    for query_i in range(len(queries)):
-        curr_query = queries[query_i]
-        for sample_i in range(len(full_database)):
-            curr_sample = full_database[sample_i]
-            p_ss = percent_shared_sites(curr_query, curr_sample)
-            try: accuracy_list[query_i].append(p_ss)
-            except IndexError: accuracy_list.append([p_ss])
-    return accuracy_list
 
-def accuracy_indices(accuracty_list):
-    '''
-    sorts list of accuracy percentages and returns the indexes of the ordered list
-    '''
-    ordered_indices = []
-    for l in accuracty_list:
-        index_ordered = np.argsort(l)
-        ordered_indices.append(index_ordered)
-    return ordered_indices
 
-def ss_vs_bf(ss_indices, bf_indices, k):
-    print('SS')
-    for ss in ss_indices:
-        print(ss[:k])
 
-    print('BF')
-    for bf in bf_indices:
-        print(bf[:k])
+# class TestPercentSharedSites(unittest.TestCase):
+#     def test_percent_shared_sites(self):
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

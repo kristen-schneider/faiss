49c1a41b1a56a745d8b04502d3980d9315042657
kristen-schneider <kristen.schneider@colorado.edu>
2022-04-28 11:23:28 -0600

    testing mwu

--- tutorial/kristen/tests/test_mann_whitney_u.py
+++ tutorial/kristen/tests/test_mann_whitney_u.py
@@ -9,19 +9,34 @@ sys.path.insert(0, parentdir)
 
 from scripts import mann_whitney_u
 
-class TestMannWhitneyU(unittest.TestCase):
+class TestMannWhitneyUSmall(unittest.TestCase):
     group1 = [1, 2, 3, 4, 5]
     group2 = [1, 2, 3, 4, 5]
     group3 = [5, 4, 3, 2, 1]
     group4 = [0, 0, 0, 0, 0]
+    group5 = [6, 7, 8, 9, 10]
+    group6 = [2, 4, 5, 1, 3]
+    random1 = np.random.randint(500, size=(100, 31))
 
-    def test_mann_whitney_u_pvalue(self):
+    def test_accept_null(self):
         self.assertEqual(mann_whitney_u.mann_whitney_u_test(self.group1, self.group2)[1], 1)
-        self.assertEqual(mann_whitney_u.mann_whitney_u_test(self.group2, self.group1)[1], 1)
         self.assertEqual(mann_whitney_u.mann_whitney_u_test(self.group1, self.group3)[1], 1)
-        self.assertEqual(mann_whitney_u.mann_whitney_u_test(self.group3, self.group1)[1], 1)
         self.assertEqual(mann_whitney_u.mann_whitney_u_test(self.group2, self.group3)[1], 1)
-        self.assertEqual(mann_whitney_u.mann_whitney_u_test(self.group3, self.group2)[1], 1)
+        self.assertGreaterEqual(mann_whitney_u.mann_whitney_u_test(self.group1, self.group6)[1], 1)
+
+class TestMannWhitneyUGenotypes(unittest.TestCase):
+    group1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    group2 = [0, 0, 1, 1, 0, 0, 0, 0, 0, 0]
+    group3 = [0, 0, 2, 1, 0, 0, 0, 2, 0, 0]
+    group4 = [0, 0, 0, 1, 0, 0, 0, 1, 0, 1]
+    group5 = [0, 0, 1, 1, 0, 0, 0, 1, 1, 1]
+
+    print(mann_whitney_u.mann_whitney_u_test(group1, group2))
+    print(mann_whitney_u.mann_whitney_u_test(group1, group3))
+    print(mann_whitney_u.mann_whitney_u_test(group1, group4))
+    print(mann_whitney_u.mann_whitney_u_test(group1, group5))
+    # print(mann_whitney_u.mann_whitney_u_test(group3, group5))
+
 
 
 
